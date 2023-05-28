df992e84afb505666a82a1e544b7de8ca52613f8
em.pestov <em.pestov@tensor.ru>
2023-05-28 17:23:27 +0300

    Homework 7

---
+++ Webinar 7/task_with_star.py
@@ -0,0 +1,87 @@
+# Напишите класс RomanNums
+# Экземпляр класса создается из строки - Римского числа.
+# Реализуйте методы класса:
+# 1. from_roman, который переводит римскую запись числа в арабскую
+# 2. is_palindrome, метод определяет, является ли арабское число палиндромом (True - является, иначе False)
+# т.е. имеет ли одинаковое значение число при чтении слева направо и справа налево
+# Например (Ввод --> Вывод) :
+# RomanNums('MMMCCLXIII').from_roman() --> 3263
+# RomanNums('CMXCIX').is_palindrome() --> True
+
+class RomanNums:
+    def __init__(self, roman_number: str):
+        """
+        Класс римские числа
+        :param roman_number: римское число
+        """
+        self.roman_number = roman_number
+
+    def from_roman(self):
+        """
+        Преобразует римское число в арабское
+        :return: арабская запись числа
+        """
+        roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        arabic_number = 0
+        for ind, num in enumerate(self.roman_number):
+            if len(self.roman_number[ind:]) == 1:
+                arabic_number += roman_dict.get(num)
+            elif roman_dict.get(num) >= roman_dict.get(self.roman_number[ind + 1]):
+                arabic_number += roman_dict.get(num)
+            else:
+                arabic_number -= roman_dict.get(num)
+        return arabic_number
+
+    def is_palindrome(self):
+        """
+        Определяет является ли арабская запись римского числа палиндромом
+        :return: True / False - является / не является
+        """
+        number = str(self.from_roman())
+        mid = len(number) // 2
+        if len(number) % 2 == 0:
+            step = 1
+        else:
+            step = 0
+        if number[:mid] == number[len(number): mid - step: -1]:
+            palindrome = True
+        else:
+            palindrome = False
+        return palindrome
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+data = [RomanNums('MMMCCLXIII').from_roman,
+        RomanNums('CXXXIV').from_roman,
+        RomanNums('LXXXVI').from_roman,
+        RomanNums('MCDV').from_roman,
+        RomanNums('CMLXXVIII').from_roman,
+        RomanNums('MMMCDIV').from_roman,
+        RomanNums('CMX').from_roman,
+        RomanNums('MMCCCLXXXVIII').from_roman,
+        RomanNums('MMVIII').from_roman,
+        RomanNums('MCLXXIX').from_roman,
+        RomanNums('MMMDCCXCV').from_roman,
+        RomanNums('CMLXXXVIII').from_roman,
+        RomanNums('CMXCIX').from_roman,
+        RomanNums('CDXLIV').from_roman,
+        RomanNums('CMXCIX').is_palindrome,
+        RomanNums('CDXLIV').is_palindrome,
+        RomanNums('MMMCCLXIII').is_palindrome,
+        RomanNums('CXXXIV').is_palindrome,
+        RomanNums('V').is_palindrome,
+        RomanNums('MI').is_palindrome,
+        RomanNums('XXX').is_palindrome,
+        RomanNums('D').is_palindrome,
+        ]
+
+
+test_data = [3263, 134, 86, 1405, 978, 3404, 910, 2388, 2008, 1179, 3795, 988, 999, 444,
+             True, True, False, False, True, True, False, False]
+
+for i, d in enumerate(data):
+    assert_error = f'Не прошла проверка для метода {d.__qualname__} экземпляра с атрибутами {d.__self__.__dict__}'
+    assert d() == test_data[i], assert_error
+    print(f'Набор для метода {d.__qualname__} экземпляра класса с атрибутами {d.__self__.__dict__} прошёл проверку')
+print('Всё ок')
